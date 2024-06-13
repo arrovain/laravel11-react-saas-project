7cca3d735a1361ffdb656affc4534d286732adbb
arrovain <arrovain@gmail.com>
2024-06-13 17:26:52 +0300

    4 tables

--- database/migrations/2024_06_12_144955_create_features_table.php
+++ database/migrations/2024_06_12_144955_create_features_table.php
@@ -19,7 +19,6 @@ return new class extends Migration
             $table->string("name");
             $table->string("description");
             $table->integer("required_credits");
-            
             $table->boolean("active")->default(true);
             $table->timestamps();
 
