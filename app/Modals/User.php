8ed6e4202c426696cc1607117054a97af74e1a99
arrovain <arrovain@gmail.com>
2024-06-17 15:28:50 +0300

    create feature 1 and feature 2 controller

--- app/Modals/User.php
+++ app/Modals/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models;
+namespace App\Modals;
 
 use App\Observers\UserObserver;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -2,7 +2,7 @@
 
 namespace App\Observers;
 
-use App\Models\User;
+use App\Modals\User;
 
 class UserObserver
 {
