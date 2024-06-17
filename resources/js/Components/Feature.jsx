3273ebc9a732089d715f04c777585d19446fc958
arrovain <arrovain@gmail.com>
2024-06-17 16:19:11 +0300

    properties

---
+++ resources/js/Components/Feature.jsx
@@ -0,0 +1,4 @@
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Head, Link, usePage } from "@inertiajs/react";
+
+export default function Feature({ feature, answer, children }) {}
