498bd5b9ce0e452a542f210a90ca85b546d4963f
arrovain <arrovain@gmail.com>
2024-06-17 14:46:43 +0300

    create another feature but this time it needs to be atleast 3 credits

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,6 +26,14 @@ class DatabaseSeeder extends Seeder
             'description' =>'Calculate sum of two number',
             'active' => true,
         ]);
+        Feature::create([
+            'image' => 'https://static-00.iconduck.com/assets.00/plus-icon-2048x2048-z6v59bd6.png',
+            'route_name' => 'feature2.index',
+            'name' => 'Calculate sum',
+            'required_credits' => 3,
+            'description' =>'Calculate sum of two number',
+            'active' => true,
+        ]);
     
 
     }
