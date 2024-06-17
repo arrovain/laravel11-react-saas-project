4e4d45f14201755c93378d10eac26fe4065a33c3
arrovain <arrovain@gmail.com>
2024-06-17 17:22:28 +0300

    svg icon from heroicons

--- resources/js/Components/Feature.jsx
+++ resources/js/Components/Feature.jsx
@@ -27,7 +27,22 @@ export default function Feature({ feature, answer, children }) {
                     <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg relative">
                         {availableCredits !== null &&
                             feature.required_credits > availableCredits && (
-                                <div className="absolute left-0 top-0 right-0 bottom-0 flex flex-col item-center justify-center bg-whşte/70 gap-3"></div>
+                                <div className="absolute left-0 top-0 right-0 bottom-0 flex flex-col item-center justify-center bg-whşte/70 gap-3">
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke-width="1.5"
+                                        stroke="currentColor"
+                                        class="size-6"
+                                    >
+                                        <path
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z"
+                                        />
+                                    </svg>
+                                </div>
                             )}
                     </div>
                 </div>
