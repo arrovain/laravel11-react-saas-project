a815ac0c084974085e2a926d2dac3bba154ec20d
arrovain <arrovain@gmail.com>
2024-06-17 14:41:32 +0300

    route_name and name as a feature

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,8 +19,10 @@ class DatabaseSeeder extends Seeder
             'password' => bcrypt('123.321Aa'),
     ]);
         Feature::create([
-            'image' => 'https://static-00.iconduck.com/assets.00/plus-icon-2048x2048-z6v59bd6.png'
-        ])
+            'image' => 'https://static-00.iconduck.com/assets.00/plus-icon-2048x2048-z6v59bd6.png',
+            'route_name' => 'feature1.index',
+            'name' => 'Calculate sum'
+        ]);
     
 
     }
