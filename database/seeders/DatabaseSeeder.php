9a56433c564a5ab97ef8b8ce26a70ab46d6d6580
arrovain <arrovain@gmail.com>
2024-06-17 14:40:19 +0300

    implement feature and add a dummy image

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Feature;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -17,6 +18,10 @@ class DatabaseSeeder extends Seeder
             'email' => 'icme@example.com',
             'password' => bcrypt('123.321Aa'),
     ]);
+        Feature::create([
+            'image' => 'https://static-00.iconduck.com/assets.00/plus-icon-2048x2048-z6v59bd6.png'
+        ])
+    
 
     }
     
