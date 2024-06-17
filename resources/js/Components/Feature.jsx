2ddaafecc77bbfb22737416d1665f49e2a43a00e
arrovain <arrovain@gmail.com>
2024-06-17 17:24:14 +0300

    "You dont have enough credits for this" section

--- resources/js/Components/Feature.jsx
+++ resources/js/Components/Feature.jsx
@@ -42,6 +42,13 @@ export default function Feature({ feature, answer, children }) {
                                             d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z"
                                         />
                                     </svg>
+                                    <div>
+                                        You dont have enough credits for this
+                                        feature. Go{""}
+                                        <Link href="/" className="underline">
+                                            Buy more credist
+                                        </Link>{" "}
+                                    </div>{" "}
                                 </div>
                             )}
                     </div>
